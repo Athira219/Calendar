475107d9853c358a6f156afad47ebe55a2b7fb60
Athira Somaraj <athirasomaraj070gmail.com>
2022-07-25 13:32:32 +0530

    style,images,script are updated

---
+++ calendar.js
@@ -0,0 +1,88 @@
+const date = new Date();
+
+const renderCalender  = () =>{ 
+    date.setDate(1);
+
+    const monthDays = 
+        document.querySelector(".days");
+
+    const lastDay = new Date(  
+         date.getFullYear(),
+         date.getMonth() + 1,
+         0
+        
+     ).getDate();
+
+
+    const prevLastDay = new Date(
+        date.getFullYear(),
+        date.getMonth(),
+        0
+    ).getDate();
+    
+    
+    const firstDayIndex = date.getDay();
+
+    const lastDayIndex = new Date(
+        date.getFullYear(),
+        date.getMonth() + 1,
+        0
+    ).getDay();
+
+    const nextDays = 7 - lastDayIndex- 1;
+
+    const months = [
+        'January','February','March','April','May','june','July','August','September','October','November','December',
+    ];
+
+    document.querySelector(".content h1"). innerHTML = 
+        months[date.getMonth()];
+
+    
+    document.querySelector(".content p"). innerHTML = 
+        new Date().toDateString();
+
+    let days = "";
+
+    for(let x = firstDayIndex; x>0; x--){
+        days +=`<div class="previous-days">
+            ${prevLastDay - x + 1}</div>` ;
+    }
+    for (let i=1; i <= lastDay; i++) {
+        if (
+            i === new Date().getDate() &&
+            date.getMonth() === new Date().getMonth()
+         ) {
+            days += `<div class="today">${i}</div>`;
+        }else{
+            days += `<div>${i}</div>`;
+        }
+    }
+
+    for (let j=1; j<=nextDays; j++) {
+        days += `<div class="next-days">${j}</div>`;
+        monthDays. innerHTML = days;     
+    }
+
+} ;    
+
+    document.querySelector(".prev")
+            .addEventListener("click", () => {
+    date.setMonth(date.getMonth() - 1);
+    renderCalender();
+     });
+
+    
+     document.querySelector(".next")
+            .addEventListener("click", () => {
+    date.setMonth(date.getMonth() + 1);
+     renderCalender();
+     });
+    renderCalender();        
+            
+    
+    
+
+
+
+
